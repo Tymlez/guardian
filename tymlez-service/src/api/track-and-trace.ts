c53bd4e22c52cf4fc409e0d9d1faa353f4bc3e1c
David Lin <dlin-me@users.noreply.github.com>
2022-03-04 10:15:03 +1100

    Revert temporary fix for invalid mrv records

--- tymlez-service/src/api/track-and-trace.ts
+++ tymlez-service/src/api/track-and-trace.ts
@@ -224,12 +224,10 @@ export const makeTrackAndTraceApi = ({
         deviceId: string | undefined;
       };
 
-      // Note: ignored the MRV because it was created by a bug with a timestamp in the future '2022-08-29T23:35:00.000Z'
       const mrv = await processedMrvRepository.findOne({
         where: {
           policyTag,
-          deviceId,
-          key: { $ne: 'TymlezCET-DD54108399431-2022-08-29T23:35:00.000Z' },
+          deviceId
         },
         order: { timestamp: 'DESC' },
       });
