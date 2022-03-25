da1ba233478df040a7cb7326ca91090149e62190
Truong Nguyen <samuraitruong@hotmail.com>
2022-03-25 16:37:32 +1100

    reverted change on a app.ts

--- api-gateway/src/app.ts
+++ api-gateway/src/app.ts
@@ -1,22 +1,22 @@
 import {
-  accountAPI,
-  trustchainsAPI,
-  demoAPI,
-  profileAPI,
-  schemaAPI,
-  tokenAPI,
-  externalAPI,
-  ipfsAPI,
+    accountAPI,
+    trustchainsAPI,
+    demoAPI,
+    profileAPI,
+    schemaAPI,
+    tokenAPI,
+    externalAPI,
+    ipfsAPI
 } from '@api/service';
-import { Guardians } from '@helpers/guardians';
+import {Guardians} from '@helpers/guardians';
 import express from 'express';
 import FastMQ from 'fastmq';
-import { createServer } from 'http';
-import { authorizationHelper } from '@auth/authorizationHelper';
+import {createServer} from 'http';
+import {authorizationHelper} from '@auth/authorizationHelper';
 import { IPFS } from '@helpers/ipfs';
-import { policyAPI } from '@api/service/policy';
-import { PolicyEngine } from '@helpers/policyEngine';
-import { WebSocketsService } from '@api/service/websockets';
+import {policyAPI} from '@api/service/policy';
+import {PolicyEngine} from '@helpers/policyEngine';
+import {WebSocketsService} from '@api/service/websockets';
 import { Users } from '@helpers/users';
 import { Wallet } from '@helpers/wallet';
 import { settingsAPI } from '@api/service/settings';
@@ -26,53 +26,44 @@ import { Logger } from 'logger-helper';
 const PORT = process.env.PORT || 3002;
 
 Promise.all([
-  FastMQ.Client.connect(
-    process.env.SERVICE_CHANNEL,
-    7500,
-    process.env.MQ_ADDRESS,
-  ),
+    FastMQ.Client.connect(process.env.SERVICE_CHANNEL, 7500, process.env.MQ_ADDRESS)
 ]).then(async ([channel]) => {
-  const app = express();
-  app.use(express.json());
-  app.use(
-    express.raw({
-      inflate: true,
-      limit: '4096kb',
-      type: 'binary/octet-stream',
-    }),
-  );
-  app.use((req, _, next) => {
-    console.log('Request:%s %s', req.method, req.url);
-    next();
-  });
-  new Logger().setChannel(channel);
-  new Guardians().setChannel(channel);
-  new IPFS().setChannel(channel);
-  new PolicyEngine().setChannel(channel);
-  new Users().setChannel(channel);
-  new Wallet().setChannel(channel);
+    const app = express();
+    app.use(express.json());
+    app.use(express.raw({
+        inflate: true,
+        limit: '4096kb',
+        type: 'binary/octet-stream'
+    }));
 
-  const server = createServer(app);
-  new WebSocketsService(server, channel);
+    new Logger().setChannel(channel);
+    new Guardians().setChannel(channel);
+    new IPFS().setChannel(channel);
+    new PolicyEngine().setChannel(channel);
+    new Users().setChannel(channel);
+    new Wallet().setChannel(channel);
 
-  ////////////////////////////////////////
+    const server = createServer(app);
+    new WebSocketsService(server, channel);
 
-  // Config routes
-  app.use('/policies', authorizationHelper, policyAPI);
-  app.use('/accounts/', accountAPI);
-  app.use('/profiles/', authorizationHelper, profileAPI);
-  app.use('/settings/', authorizationHelper, settingsAPI);
-  app.use('/schemas', authorizationHelper, schemaAPI);
-  app.use('/tokens', authorizationHelper, tokenAPI);
-  app.use('/trustchains/', authorizationHelper, trustchainsAPI);
-  app.use('/external/', externalAPI);
-  app.use('/demo/', demoAPI);
-  app.use('/ipfs', authorizationHelper, ipfsAPI);
-  app.use('/logs', authorizationHelper, loggerAPI);
-  /////////////////////////////////////////
+    ////////////////////////////////////////
 
-  server.listen(PORT, () => {
-    new Logger().info(`Started on ${PORT}`, ['API_GATEWAY']);
-    console.log('UI service started on', PORT);
-  });
+    // Config routes
+    app.use('/policies', authorizationHelper, policyAPI);
+    app.use('/accounts/', accountAPI);
+    app.use('/profiles/', authorizationHelper, profileAPI);
+    app.use('/settings/', authorizationHelper, settingsAPI);
+    app.use('/schemas', authorizationHelper, schemaAPI);
+    app.use('/tokens', authorizationHelper, tokenAPI);
+    app.use('/trustchains/', authorizationHelper, trustchainsAPI);
+    app.use('/external/', externalAPI);
+    app.use('/demo/', demoAPI);
+    app.use('/ipfs', authorizationHelper, ipfsAPI);
+    app.use('/logs', authorizationHelper, loggerAPI);
+    /////////////////////////////////////////
+
+    server.listen(PORT, () => {
+        new Logger().info(`Started on ${PORT}`, ['API_GATEWAY']);
+        console.log('UI service started on', PORT);
+    });
 });
